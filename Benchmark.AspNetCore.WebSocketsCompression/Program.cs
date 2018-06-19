e5ad93f956f5c09cc54b92c07ad3a77772ad1602
Tomasz Pęczek <tpeczek@gmail.com>
2018-06-19 20:51:23 +0200

    Benchmark Project

---
+++ Benchmark.AspNetCore.WebSocketsCompression/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Reports;
+using BenchmarkDotNet.Running;
+using Benchmark.AspNetCore.WebSocketsCompression.Benchmarks;
+
+namespace Benchmark.AspNetCore.WebSocketsCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Summary compressionWebSocketSummary = BenchmarkRunner.Run<CompressionWebSocketBenchmarks>();
+
+            System.Console.ReadKey();
+        }
+    }
+}
